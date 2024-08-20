438b8edf4eab992461908955425cea199c8d8bd8
thomas kang <mckang@nhndata.com>
2024-08-20 15:14:32 +0900

    change title

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <div className="header">
-      <h1 className="project-title">Monolith - TodoApp</h1>
+      <h1 className="project-title">Monolith App - Todo</h1>
     </div>
   );
 }
